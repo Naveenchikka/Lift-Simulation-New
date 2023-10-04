6a4de3960ca6df8e5b730d2bf3c27a6155f3f7fa
Naveen Chikkamath <35519706+Naveenchikka@users.noreply.github.com>
2023-10-04 15:26:18 +0530

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -384,7 +384,7 @@ window.addEventListener('DOMContentLoaded', () => {
 const checkDeviceModeChange = () => {
   const isDeviceModeChanged = window.matchMedia("(hover: none)").matches;
 
-  if (isDeviceModeChanged && !document.activeElement.tagName.toLowerCase().match(/input|textarea/div|button/)) {
+  if (isDeviceModeChanged && !document.activeElement.tagName.toLowerCase().match(/input|textarea|div|button/)) {
       location.reload(); // Reload the page
   }
 };
